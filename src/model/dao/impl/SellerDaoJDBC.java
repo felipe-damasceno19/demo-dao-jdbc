5ad33781955475fb77cdfee8a13c0147570dfa79
felipe-damasceno19 <felipegpt19@gmail.com>
2025-09-29 08:10:49 -0300

    Insert implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,5 +1,6 @@
 package model.dao.impl;
 
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,6 +12,7 @@ import java.util.Map;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -25,7 +27,41 @@ public class SellerDaoJDBC implements SellerDao {
 	
 	@Override
 	public void insert(Seller sell) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("INSERT INTO seller "
+									   + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+									   + "VALUES "
+									   + "(?, ?, ?, ? ,?)",  
+									   Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, sell.getName());
+			st.setString(2, sell.getEmail());
+			st.setDate(3, new java.sql.Date(sell.getBirthDate().getTime()));
+			st.setDouble(4, sell.getBaseSalary());
+			st.setInt(5, sell.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					sell.setId(id);
+				}
+				DB.closeResultSet(rs);
+			}
+			else {
+				throw new DbException("Unexpected error! no rows affected");
+			}
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			
+		}
 		
 	}
 
